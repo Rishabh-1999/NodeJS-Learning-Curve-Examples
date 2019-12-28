6b13f8dc340ec9e4deff28706a932d90bbab059b
Rishabh-1999 <rishabhanand33@gmail.com>
2019-12-28 09:54:18 +0530

    Updated Package

--- Third Assignment - Manage Product Using Express Node/app.js
+++ Third Assignment - Manage Product Using Express Node/app.js
@@ -4,7 +4,7 @@ var app = express();
 var http = require('http');
 
 app.post("/putInCart", function (request, response) {
-	addToFileCart(request, function() {
+	addToFileCart(request, function () {
 		response.end();
 	})
 })
@@ -14,53 +14,52 @@ function addToFileCart(request, callback) {
 	console.log(request.responseText)
 
 	var body = '';
-	 	request.on('data', function(chunk) {
-			body += chunk;
-		});
+	request.on('data', function (chunk) {
+		body += chunk;
+	});
 
-	var obj=[];
+	var obj = [];
 
-	request.on('end', function() {
-			console.log(body)
-			var temp = JSON.parse(body);
-			obj=temp;
-		});
+	request.on('end', function () {
+		console.log(body)
+		var temp = JSON.parse(body);
+		obj = temp;
+	});
 
-	var path="./manage/cart"+obj[0].ownerName+".txt";
-	var flag=0;
+	var path = "./manage/cart" + obj[0].ownerName + ".txt";
+	var flag = 0;
 
 	try {
-  		if (fs.existsSync(path)) {
-    	flag=1;
-  		
-	} catch(err) {
-  			console.error(err)
-  		}
+		if (fs.existsSync(path)) {
+			flag = 1;
 
-  		if(flag==0)
-  		{
-  			var createStream = fs.createWriteStream(path);
-			createStream.end();
-  		}
+		}
+	} catch (err) {
+		console.error(err)
+	}
 
-	fs.readFile(path,  function(error, oldData) {
-		if(error) {
+	if (flag == 0) {
+		var createStream = fs.createWriteStream(path);
+		createStream.end();
+	}
+
+	fs.readFile(path, function (error, oldData) {
+		if (error) {
 			throw error;
 		}
-		if(oldData.toString())
+		if (oldData.toString())
 			newData = JSON.parse(oldData);
 
-		request.on('end', function() {
+		request.on('end', function () {
 			console.log(body)
 
 			var data = JSON.parse(body);
-			fs.truncate(path,0,function()
-			{
+			fs.truncate(path, 0, function () {
 				console.log('deleted');
 			});
 			newData.push(data[0]);
 			fs.writeFile(path, JSON.stringify(newData), function (error) {
-				if(error) {
+				if (error) {
 					throw error;
 				}
 			});
@@ -69,7 +68,7 @@ function addToFileCart(request, callback) {
 }
 
 app.post("/currAcc", function (request, response) {
-	addToFileCurrAcc(request, function() {
+	addToFileCurrAcc(request, function () {
 		response.end();
 	})
 })
@@ -77,26 +76,25 @@ app.post("/currAcc", function (request, response) {
 function addToFileCurrAcc(request, callback) {
 	var newData = [];
 	console.log(request.responseText)
-	fs.readFile("./manage/currAcc.txt",  function(error, oldData) {
-		if(error) {
+	fs.readFile("./manage/currAcc.txt", function (error, oldData) {
+		if (error) {
 			throw error;
 		}
-		if(oldData.toString())
+		if (oldData.toString())
 			newData = JSON.parse(oldData);
 		var body = '';
-	 	request.on('data', function(chunk) {
+		request.on('data', function (chunk) {
 			body += chunk;
 		});
-		request.on('end', function() {
+		request.on('end', function () {
 			console.log(body)
 			var data = JSON.parse(body);
-			fs.truncate('./manage/currAcc.txt',0,function()
-			{
+			fs.truncate('./manage/currAcc.txt', 0, function () {
 				console.log('deleted');
 			});
 			newData.push(data[0]);
 			fs.writeFile("./manage/currAcc.txt", JSON.stringify(newData), function (error) {
-				if(error) {
+				if (error) {
 					throw error;
 				}
 			});
@@ -105,7 +103,7 @@ function addToFileCurrAcc(request, callback) {
 }
 
 app.post("/productDetails", function (request, response) {
-	addToFileproductDetails(request, function() {
+	addToFileproductDetails(request, function () {
 		response.end();
 	})
 })
@@ -113,24 +111,24 @@ app.post("/productDetails", function (request, response) {
 function addToFileproductDetails(request, callback) {
 	var newData = [];
 	console.log(request.responseText)
-	fs.readFile("./manage/productDetails.txt",  function(error, oldData) {
-		if(error) {
+	fs.readFile("./manage/productDetails.txt", function (error, oldData) {
+		if (error) {
 			throw error;
 		}
-		if(oldData.toString())
+		if (oldData.toString())
 			newData = JSON.parse(oldData);
 		var body = '';
-	 	request.on('data', function(chunk) {
+		request.on('data', function (chunk) {
 			body += chunk;
 		});
 
-		request.on('end', function() {
+		request.on('end', function () {
 			console.log(body)
 
 			var data = JSON.parse(body);
 			newData.push(data[0]);
 			fs.writeFile("./manage/productDetails.txt", JSON.stringify(newData), function (error) {
-				if(error) {
+				if (error) {
 					throw error;
 				}
 			});
@@ -139,83 +137,85 @@ function addToFileproductDetails(request, callback) {
 }
 
 app.get("/currAcc", function (request, response) {
-fs.readFile('./manage/currAcc.txt', function(error, contents) 
-  {
-    if (error) 
-      throw error;
+	fs.readFile('./manage/currAcc.txt', function (error, contents) {
+		if (error)
+			throw error;
 
-    var tasks;
-    if (contents.length === 0) 
-      tasks = [];
-    else 
-      tasks = JSON.parse(contents);
-    login=tasks;
-    console.log(tasks);
-    	response.writeHead(200, {'Content-type': 'application/json'});
-        response.write(JSON.stringify(tasks));
-        response.end();
-  });
+		var tasks;
+		if (contents.length === 0)
+			tasks = [];
+		else
+			tasks = JSON.parse(contents);
+		login = tasks;
+		console.log(tasks);
+		response.writeHead(200, {
+			'Content-type': 'application/json'
+		});
+		response.write(JSON.stringify(tasks));
+		response.end();
+	});
 })
 
 app.get("/login", function (request, response) {
-fs.readFile('./manage/login.txt', function(error, contents) 
-  {
-    if (error) 
-      throw error;
+	fs.readFile('./manage/login.txt', function (error, contents) {
+		if (error)
+			throw error;
 
-    var tasks;
-    if (contents.length === 0) 
-      tasks = [];
-    else 
-      tasks = JSON.parse(contents);
-    login=tasks;
-    console.log(tasks);
-    	response.writeHead(200, {'Content-type': 'application/json'});
-        response.write(JSON.stringify(tasks));
-        response.end();
-  });
+		var tasks;
+		if (contents.length === 0)
+			tasks = [];
+		else
+			tasks = JSON.parse(contents);
+		login = tasks;
+		console.log(tasks);
+		response.writeHead(200, {
+			'Content-type': 'application/json'
+		});
+		response.write(JSON.stringify(tasks));
+		response.end();
+	});
 })
 
 app.get("/productDetails", function (request, response) {
-fs.readFile('./manage/productDetails.txt', function(error, contents) 
-  {
-    if (error) 
-      throw error;
+	fs.readFile('./manage/productDetails.txt', function (error, contents) {
+		if (error)
+			throw error;
 
-    var tasks;
-    if (contents.length === 0) 
-      tasks = [];
-    else 
-      tasks = JSON.parse(contents);
-    login=tasks;
-    console.log(tasks);
-    	response.writeHead(200, {'Content-type': 'application/json'});
-        response.write(JSON.stringify(tasks));
-        response.end();
-  });
+		var tasks;
+		if (contents.length === 0)
+			tasks = [];
+		else
+			tasks = JSON.parse(contents);
+		login = tasks;
+		console.log(tasks);
+		response.writeHead(200, {
+			'Content-type': 'application/json'
+		});
+		response.write(JSON.stringify(tasks));
+		response.end();
+	});
 })
 
 app.get("/login", function (request, response) {
-fs.readFile('./manage/currAcc.txt', function(error, contents) 
-  {
-    if (error) 
-      throw error;
+	fs.readFile('./manage/currAcc.txt', function (error, contents) {
+		if (error)
+			throw error;
 
-    var tasks;
-    if (contents.length === 0) 
-      tasks = [];
-    else 
-      tasks = JSON.parse(contents);
-    login=tasks;
-    console.log(tasks);
-    	response.writeHead(200, {'Content-type': 'application/json'});
-        response.write(JSON.stringify(tasks));
-        response.end();
-  });
+		var tasks;
+		if (contents.length === 0)
+			tasks = [];
+		else
+			tasks = JSON.parse(contents);
+		login = tasks;
+		console.log(tasks);
+		response.writeHead(200, {
+			'Content-type': 'application/json'
+		});
+		response.write(JSON.stringify(tasks));
+		response.end();
+	});
 })
 
 app.use(express.static('manage'));
 app.listen(3000);
 console.log("Running on localhost")
-
-
